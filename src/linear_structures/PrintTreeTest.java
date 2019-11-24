8ac95dd8f252a22d3f2b39144bdf1afebe80917d
markfrias <friasmarkjabel@gmail.com>
2019-11-24 23:56:44 +0800

    Finalize test file for Tree.java

--- src/linear_structures/PrintTreeTest.java
+++ src/linear_structures/PrintTreeTest.java
@@ -3,19 +3,31 @@ package linear_structures;
 public class PrintTreeTest {
     public static void main(String[] args){
         Tree<Integer> tree = new Tree<>();
+        int space = 0;
 
-        tree.insertNode(2);
-        tree.insertNode(3);
-        tree.insertNode(7);
-        tree.insertNode(21);
-        tree.insertNode(9);
+        tree.insertNode(49);
+        tree.insertNode(28);
+        tree.insertNode(83);
+        tree.insertNode(18);
+        tree.insertNode(40) ;
+        tree.insertNode(11);
+        tree.insertNode(19);
+        tree.insertNode(32);
+        tree.insertNode(44);
+        tree.insertNode(71);
+        tree.insertNode(97);
+        tree.insertNode(49);
+        tree.insertNode(69);
+        tree.insertNode(72);
+        tree.insertNode(92);
+        tree.insertNode(99);
 
 
 
 
 
 
-        tree.outputTree(1, tree.getRoot());
+        tree.outputTree(space, tree.getRoot());
 
     }
 }
